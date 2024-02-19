cbfc69f39129ebe46c29372595d8c78117091ca7
Aseef <100977702+aseef2289@users.noreply.github.com>
2024-02-18 20:24:57 -0500

    Delete src directory

--- src/components/Desktop1/Ellipse694Icon.tsx
+++
@@ -1,9 +0,0 @@
-import { memo, SVGProps } from 'react';
-
-const Ellipse694Icon = (props: SVGProps<SVGSVGElement>) => (
-  <svg preserveAspectRatio='none' viewBox='0 0 808 650' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-    <ellipse cx={404} cy={325} rx={404} ry={325} fill='#FFE3A2' />
-  </svg>
-);
-const Memo = memo(Ellipse694Icon);
-export { Memo as Ellipse694Icon };
--- src/components/Desktop1/Ellipse695Icon.tsx
+++
@@ -1,9 +0,0 @@
-import { memo, SVGProps } from 'react';
-
-const Ellipse695Icon = (props: SVGProps<SVGSVGElement>) => (
-  <svg preserveAspectRatio='none' viewBox='0 0 808 650' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-    <ellipse cx={404} cy={325} rx={404} ry={325} fill='#E09623' />
-  </svg>
-);
-const Memo = memo(Ellipse695Icon);
-export { Memo as Ellipse695Icon };
