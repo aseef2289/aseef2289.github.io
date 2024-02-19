3bfe97495fb4308eb4705158a850b346388bc9a8
Aseef <100977702+aseef2289@users.noreply.github.com>
2024-02-18 20:18:00 -0500

    Add files via upload

---
+++ src/components/Desktop1/Ellipse693Icon.tsx
@@ -0,0 +1,9 @@
+import { memo, SVGProps } from 'react';
+
+const Ellipse693Icon = (props: SVGProps<SVGSVGElement>) => (
+  <svg preserveAspectRatio='none' viewBox='0 0 808 650' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <ellipse cx={404} cy={325} rx={404} ry={325} fill='#F4C76A' />
+  </svg>
+);
+const Memo = memo(Ellipse693Icon);
+export { Memo as Ellipse693Icon };
